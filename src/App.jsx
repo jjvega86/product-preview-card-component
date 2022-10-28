c15517cdbd072b5be28d0107edffd0682e81a45d
JJ <jj.vega86@gmail.com>
2022-10-28 06:35:39 -0400

    📦 NEW: Add Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       Preview Gabrielle Essence Eau De Parfum A floral, solar and voluptuous
       interpretation composed by Olivier Polge, Perfumer-Creator for the House
       of CHANEL. $149.99 $169.99 Add to Cart
-      <div class="attribution">
+      <div>
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
